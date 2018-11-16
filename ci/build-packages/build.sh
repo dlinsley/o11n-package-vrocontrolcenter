b077bbab2243d511468fd6dacecb93b99d7b0a77
Daniel Linsley <dlinsley@vmware.com>
2018-11-15 16:19:14 -0800

    fix version file

--- ci/build-packages/build.sh
+++ ci/build-packages/build.sh
@@ -5,7 +5,7 @@
 for POM in $(ls */pom.xml)
 do
   echo "Building $POM"
-  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o versions:set -DnewVersion=$(cat o11n-package-vroremote-version/version)
+  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o versions:set -DnewVersion=$(cat o11n-package-vrocontrolcenter-version/version)
   mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o package -PconcourseDistOut
   if [ $? -eq 0 ]; then
     echo -e "=== Successfully built package $POM ==="
